e96788b930822e7543fabf1276cff38c207930de
Kamjar Gerami <kami.gerami@gmail.com>
2018-11-18 15:11:43 +0100

    terraform: add s3 backend storage for statefile and hard versions for providers

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
+  version = "~> 1.41"
+}
+
+provider "archive" {
+  version = "~> 1.0"
 }
 
 # get accountID
@@ -29,4 +34,13 @@ data "archive_file" "message_action" {
   type        = "zip"
   source_file = "${path.module}/lambda/message_action/message_action.py"
   output_path = "${path.module}/.terraform/archive_files/message_action.zip"
+}
+
+# remote store on s3
+terraform {
+  backend "s3" {
+    bucket = "terraform.codelabs.se"
+    key    = "slack/notify_to_prod/"
+    region = "us-east-1"
+  }
 }
